bbac18071c0e620d74d6d81f7e9552118a254c95
EstyBrandes <95626359+EstyBrandes@users.noreply.github.com>
2022-12-16 22:30:05 +0200

    Update monster.h

--- monster.h
+++ monster.h
@@ -5,24 +5,33 @@ class Monster
 public:
 
 	// Esty:
-	Monster();
+	Monster(char* n);
 	~Monster();
 
 	int getLevel();
 	char* getName();
+	int operator++();
 
 
 	// operator++
 
 private:
-	const char* name;
-	int level = 1;
+	char* name;
+	int level;
 };
 
-Monster::Monster()
+
+Monster::Monster(char* n)
 {
+	name = new char[strlen(n) + 1];
+	if (name == nullptr)
+		delete[] name;
+	strncpy(name, n, strlen(n) + 1);
+	level = 1;
+
 }
 
 Monster::~Monster()
 {
+	delete[] name;
 }

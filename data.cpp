d501f831a559080ac8bc0273fe42662fe4325597
EstyBrandes <95626359+EstyBrandes@users.noreply.github.com>
2022-12-19 10:41:33 +0200

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -1,36 +1,100 @@
-#pragma once
 
-#include "map.h"
-#include "room.h"
+#include "game.h"
 
-class Data
+Game::Game()
 {
-public:
+	currentMap = nullptr;
+	data = new Data();
+}
 
-	// Almog
-	Data();
-	~Data();
+Game::~Game()
+{
+	delete data;
+}
 
-	// Almog:
-	Room* addRoom(char* name);
-	Map* addMap(char* name);
+Map* Game::addMap(char* m_name)
+{
+	if (!m_name) return nullptr;
+	Map* temp_map = data->getMap(m_name);
+	if (temp_map)
+	{
+		return nullptr;
+	}
+	return temp_map;
+}
+
+Room* Game::addRoomToData(char* r_name)
+{
+	if (!r_name) return nullptr;
+	return data->addRoom(r_name);
+}
+
+Map* Game::addRoomToMap(char* r_name, Direction direction = None)
+{
+	if (!r_name) return nullptr;
 	
-	// Esty:
-	bool addItem(char* r_name, char* i_name);
-	bool addMonster(char* r_name, char* m_name);
+	Room* newRoom = data->getRoom(r_name);
+	if (!newRoom) return nullptr;
+	
+	if (!currentMap->addRoom(newRoom, direction)) return nullptr;
+	else return currentMap;
+}
 
-	// Esty:
-	Map* getMap(char* m_name) const;
+void Game::printCurrMap()
+{
+	currentMap->printMap();
+}
 
-	// Almog:
-	Room* getRoom(char* r_name) const;
+Map* Game::changeCurrMap(char* m_name)
+{
+	if (!m_name) return nullptr;
 
-	// Esty:
-	void printData(); 
-private:
-	int num_of_rooms;
-	int num_of_maps;
+	Map* temp_map = data->getMap(m_name);
+	if (temp_map)
+	{
+		return nullptr;
+	}
+	return temp_map;
+}
 
-	Map* maps;
-	Room* rooms;
-};
+Map* Game::CombineCurrMapToMap(char* m_name)
+{
+	Map* temp_m = data->getMap(m_name);
+	if (!temp_m) return nullptr;
+	*currentMap += *temp_m;
+	return currentMap;
+}
+
+// no empty map constructors are pain...
+Map* Game::CombineMapsToNew(char* new_m_name, char* m_name)
+{
+	Map* temp_m = data->getMap(m_name);
+	if (!temp_m) return nullptr;
+
+
+	Map* combined = new Map(new_m_name);
+
+}
+
+void Game::printData()// prints only maps and rooms names.
+{
+	data->printData();
+}
+
+bool Game::addItemToRoom(char* i_name) 
+{
+	return currentMap->addItemToCurrRoom(i_name);
+}
+bool Game::addMonsterToRoom(char* m_name)
+{
+	return currentMap->addMosterToCurrRoom(m_name);
+}
+bool Game::addItemToDataRoom(char* r_name, char* i_name)
+{
+	return data->addItem(r_name, i_name);
+}
+bool Game::addMonsterToDataRoom(char* r_name, char* m_name)
+{
+	return data->addMonster(r_name, m_name);
+}
+
